a57cf13154d8fcbce104b8a1046140258487c104
Nathan Hull <hulln@arch05.cis.gvsu.edu>
2018-11-27 17:01:32 -0500

    Works for the right amount of nodes, or more nodes than the dim

--- kqueen.cpp
+++ kqueen.cpp
@@ -21,43 +21,37 @@ bool checkPos (int row, int col, int board[]) {
 
 	for (i = 0; i < col; i++) {
 		// same row
-		if (board[i] == row) {
+		if (board[i] == row)
 			isValid = false;
-			printf("Same row used %i %i\n", board[i], i);
-		}
 		// forwardslash diagonal
-		else if (board[i] + i == row + col) {
+		else if (board[i] + i == row + col)
 			isValid = false;
-			printf("Forwardslash %i %i\n", board[i], i);
-		}
 		// backslash diagonal
-		else if (board[i] - i == row - col) {
+		else if (board[i] - i == row - col)
 			isValid = false;
-			printf("Backwardslash %i %i\n", board[i], i);
-		}
 	}
-	printf("CHECKED %ix%i is %i\n", col, row, isValid);
 
 	return isValid;
 }
 
 
 int checkRoutes (int row, int col, int board[], int dim) {
+	int i;
+
 	// Have reached the end
-	if (col == dim - 1)
+	if (col == dim - 1) {
+		for (i = 0; i < dim; i++) {
+			printf("%i ", board[i]);
+		}
+		printf("\n");
 		return 1;
+	}
 
 	int solutions = 0;
-	int i;
 	int nextCol = col + 1;
 
 	for (i = 0; i < dim; i++) {
 		if (checkPos (i, nextCol, board)) {
-		int x;
-		for (x = 0; x < dim; x++) {
-			printf("%i ", board[x]);
-		}
-		printf("\n");
 			board[nextCol] = i;
 			solutions += checkRoutes (i, nextCol, board, dim);
 			board[nextCol] = 0;
@@ -89,51 +83,54 @@ int main (int argc, char** argv) {
 	int buffer;
 	int solutions = 0;
 
+	// Serial implementation
 	if (num_nodes == 1) {
 		printf ("Checking %i queens on a %iX%i board\n", dim, dim, dim);
 
 		for (i = 0; i < dim; i++) {
 			int* board = (int*) malloc (dim * sizeof(int));
 			board[0] = i;
-			buffer = i;
-			solutions += checkRoutes (buffer, 0, board, dim);
+			solutions += checkRoutes (i, 0, board, dim);
 			free (board);
 		}
+		printf("Number of solutions: %i\n", solutions);
+
 	}
+
+
+
+	// Distributed implementation
 	else {
+		// too large of a board, need to split up work
+		if (num_nodes < dim) {
+
+		}
+		// otherwise can just assign one row to each node
+		else {
+			if (rank < dim) {
+				char host[25];
+				gethostname(host, 25);
+				printf("Host %s checking %i\n", host, rank);
+				int* board = (int*) malloc(dim * sizeof(int));
+				board[0] = rank;
+				solutions = checkRoutes (rank, 0, board, dim);
+				free (board);
+			}
+		}
+		
 		if (rank == MASTER) {
+			for (i = 1; i < dim; i++) {
+				MPI_Recv (&buffer, 1, MPI_INT, i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				solutions += buffer;
+			}
 			printf ("Checking %i queens on a %iX%i board\n", dim, dim, dim);
+			printf("Number of solutions: %i\n", solutions);
 
-			// too large of a board, need to split up work
-			if (num_nodes < dim) {
-
-			}
-			// otherwise can just assign one row to each node
-			else {
-				for (i = 0; i < dim; i++) {
-					buffer = dim;
-					MPI_Send (&buffer, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
-				}
-			}
 		} else {
-			MPI_Recv (&buffer, 1, MPI_INT, MASTER, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			char host[25];
-			gethostname(host, 25);
-			printf("Process %s received %i\n", host, buffer);
-			fflush(stdout);
-
-			int* board = (int*) malloc(dim * sizeof(int));
-			board[0] = buffer;
-			
-			solutions += checkRoutes (buffer, 0, board, dim);
-			free (board);
+			MPI_Send (&solutions, 1, MPI_INT, MASTER, TAG, MPI_COMM_WORLD);
 		}
 	}
 
-	MPI_Barrier(MPI_COMM_WORLD);
-	printf("Number of solutions: %i\n", solutions);
 
 	MPI_Finalize ();
 }
-
-
